b00936368741f776c985cb5086fc0bda61284729
TheOssumOpossum <v.bommier@gmail.com>
2020-12-09 01:17:59 -0500

    paginate long messages, only works for code blocks

--- src/discord_api.py
+++ src/discord_api.py
@@ -1,2 +1,25 @@
 async def send_message(text, channel):
-    await channel.send(text)
+    if len(text) > 1950:
+        page = 0
+        last_num = 3
+        yes_break = False
+        while True:
+            for i in range(last_num+1950,-1,-1):
+                if i >= len(text):
+                    yes_break = True
+                    break
+                if text[i] == '\n':
+                    page += 1
+                    await channel.send("```\n" + "page{0}".format(page) + text[last_num:i] + "\n```")
+                    last_num = i
+                    break
+                if i == last_num:
+                    print("returning early no new lines to splice")
+                    yes_break = True
+                    break
+            if yes_break:
+                page += 1
+                await channel.send("```\n"+ "page{0}".format(page) + text[last_num:-4] + "\n```")
+                break
+    else:
+        await channel.send(text)
