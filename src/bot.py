af163be0804df4ff8cb3296d1f5383803822af94
TheOssumOpossum <v.bommier@gmail.com>
2020-12-08 23:31:41 -0500

    multiple server pings and react for roles in get-roles channel

--- src/bot.py
+++ src/bot.py
@@ -4,18 +4,67 @@ from constants import DISCORD_CLIENT_ID
 import timecog
 from discord_message_handler import handle_message
 
+role_name_to_emoji = {'advent of code':'😂'}
+
+def setup_gaer_dictionary(): #guild and emoji to role
+    for guild in client.guilds:
+        for role in guild.roles:
+            role_name = role.name.lower()
+            if role_name in role_name_to_emoji.keys():
+                emoji = role_name_to_emoji[role_name]
+                if emoji:
+                    gaer[(guild.id, emoji)] = role.id
+    # print(gaer)
+
 async def on_ready():
     print('discordbot... Logged on as {0}!'.format(client.user))
-    await client.change_presence(status=discord.Status.invisible)
+    client.cogs['Heartbeat'].setup_advent_blasts()
+    client.intents.guild_reactions = False
+    client.intents.members = True
+    setup_gaer_dictionary()
+    await client.change_presence(status=discord.Status.do_not_disturb)
 
 async def on_message(message):
-    if message.author.name != client.user.name:
+    if message.author.id != client.user.id:
         print('discordbot... Message from {0.author}: {0.content}'.format(message))
         await handle_message(message)
 
-client = commands.Bot(command_prefix=commands.when_mentioned_or('~'))
+intents = discord.Intents.default()
+# intents.members = True
+# intents.guild_reactions = True
+
+client = commands.Bot(command_prefix=commands.when_mentioned_or('~'), intents=intents)
 client.add_listener(on_ready)
 client.add_listener(on_message)
 client.load_extension('timecog')
+
+#Guild And Emoji to Role
+gaer = {}
+
+@client.event
+async def on_raw_reaction_add(payload):
+    # print(payload)
+    channel = client.get_channel(payload.channel_id)
+    guild = client.get_guild(payload.guild_id)
+    if (channel and channel.name.lower() == "get-roles"):
+        emoji = payload.emoji.name
+        role_id = gaer[(payload.guild_id), emoji]
+        if role_id:
+            role = guild.get_role(role_id)
+            await payload.member.add_roles(role)
+
+# @client.event
+# async def on_raw_reaction_remove(payload):
+#     print(payload)
+#     channel = client.get_channel(payload.channel_id)
+#     guild = client.get_guild(payload.guild_id)
+#     if (channel and channel.name.lower() == "get-roles"):
+#         emoji = payload.emoji.name
+#         role_id = gaer[(payload.guild_id), emoji]
+#         if role_id:
+#             role = guild.get_role(role_id)
+#             member = guild.get_member(payload.user_id)
+#             print(member)
+#             await payload.member.remove_roles(role)
 
 client.run(DISCORD_CLIENT_ID)
--- src/timecog.py
+++ src/timecog.py
@@ -7,8 +7,7 @@ class Heartbeat(commands.Cog):
    def __init__(self, bot):
       self.bot = bot
       self.heartbeat.start()
-      self.msg_channel = '' #only works for one guild
-      self.mention_role = '' #only works for one guild
+      self.advent_blasts = {} #role -> channel
 
    def cog_unload(self):
       self.heartbeat.stop()
@@ -18,25 +17,28 @@ class Heartbeat(commands.Cog):
       now = datetime.now(TIMEZONE)
       minute = now.minute
       hour = now.hour
-      if now.hour == 23 and now.minute == 30:
-         if self.msg_channel == '':
-            self.msg_channel = get_channel(self.bot.get_all_channels(), "advent-of-code-2020")
-            for g in self.bot.guilds:
-               for r in g.roles:
-                  if r.name.lower() == 'advent of code':
-                     self.mention_role = r.id
-         await send_message("<@&{0}> starts in 30 minutes!".format(self.mention_role), self.msg_channel)
+      if now.hour == 23 and now.minute == 30 and ((now.day < 25 and now.month == 12) or (now.day == 30 and now.month == 11)):
+         for role, channel in self.advent_blasts.items():
+            await send_message("<@&{0}> starts in 30 minutes!".format(role), channel)
 
    @heartbeat.before_loop
    async def before_update_loop(self):
       await self.bot.wait_until_ready()
 
+   def setup_advent_blasts(self):
+      for guild in self.bot.guilds:
+            for channel in guild.channels:
+               if channel.name.lower() == "advent-of-code-2020":
+                  send_to_channel = channel
+                  break
+            for role in guild.roles:
+               if role.name.lower() == 'advent of code':
+                  send_to_role = role.id
+                  break
+            if send_to_channel and send_to_role:
+               self.advent_blasts[send_to_role] = send_to_channel
+      # print(self.advent_blasts)
+
 
 def setup(bot):
     bot.add_cog(Heartbeat(bot))
-
-def get_channel(channels, channel_name):
-    for channel in channels:
-        if channel.name == channel_name:
-            return channel
-    return None
